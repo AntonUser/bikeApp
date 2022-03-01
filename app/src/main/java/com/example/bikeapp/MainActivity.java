945729f65571563ed879503db0f8e7b59ecb2798
AntonUser <titovanton5@mail.ru>
2022-03-01 13:48:11 +0600

    Add bluetooth connect

--- app/src/main/java/com/example/bikeapp/MainActivity.java
+++ app/src/main/java/com/example/bikeapp/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.bikeapp;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 
 import androidx.activity.result.ActivityResultLauncher;
@@ -15,15 +17,25 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import java.util.Set;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     private final String TAG = "bikeApp";
     private BluetoothAdapter bluetoothAdapter;
+    private boolean success = false;
+    private ConnectThread connectThread;
+    public static BluetoothSocket mmSocket;
+    public static Handler handler;
+    private ConnectedThread connectedThread;
     private ActivityResultLauncher<Intent> mStartForResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
             result -> {
             });
 
+    private final static int CONNECTING_STATUS = 1; // used in bluetooth handler to identify message status
+    private final static int MESSAGE_READ = 2; // used in bluetooth handler to identify message update
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,24 +43,32 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "OnCreate");
         setContentView(R.layout.activity_main);
         setUpNavigation();
-       // BluetoothDevice device = null;
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (bluetoothAdapter == null) {
             Log.d(TAG, "Устройство не поддерживает bluetooth");
         }
-        enableBt();
-//        Set<BluetoothDevice> bluetoothDevices = bluetoothAdapter.getBondedDevices();
-//        for (BluetoothDevice bluetoothDevice : bluetoothDevices) {
-//            if (bluetoothDevice.getName().equals("BTMR-6313")) {
-//                device = bluetoothDevice;
-//                break;
-//            }
-//        }
-        // BluetoothGatt gatt = device.connectGatt(this, true, bluetoothGattCallback, TRANSPORT_LE);
-//        gatt.connect();
+        //   MyUUID.initializeUUID();
+        for (BluetoothDevice bluetoothDevice : bluetoothAdapter.getBondedDevices()) {
+            if (bluetoothDevice.getName().equals("BTMR-6313")) {
+                connectThread = new ConnectThread(bluetoothAdapter, bluetoothDevice.getAddress());
+                break;
+            }
+        }
+        connectThread.run();
     }
 
-    public void setUpNavigation() {
+
+    @Override
+    protected void onStart() {
+        if (!bluetoothAdapter.isEnabled()) {
+            Log.d(TAG, "Запрос включения bluetooth");
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            mStartForResult.launch(enableBtIntent);
+        }
+        super.onStart();
+    }
+
+    private void setUpNavigation() {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.fragment);
@@ -57,12 +77,87 @@ public class MainActivity extends AppCompatActivity {
                 navHostFragment.getNavController());
     }
 
-    private void enableBt() {
-        if (!bluetoothAdapter.isEnabled()) {
-            Log.d(TAG, "Запрос включения bluetooth");
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            mStartForResult.launch(enableBtIntent);
+    private class ConnectThread extends Thread {
+        public ConnectThread(BluetoothAdapter bluetoothAdapter, String address) {
+            BluetoothDevice bluetoothDevice = bluetoothAdapter.getRemoteDevice(address);
+            BluetoothSocket tmp = null;
+            UUID uuid = bluetoothDevice.getUuids()[0].getUuid();
+            try {
+                tmp = bluetoothDevice.createInsecureRfcommSocketToServiceRecord(uuid);
+            } catch (IOException ex) {
+                Log.e(TAG, "Socket's create() method failed", ex);
+            }
+            mmSocket = tmp;
+        }
+
+
+        public void run() {
+            BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            bluetoothAdapter.cancelDiscovery();
+            try {
+                // Connect to the remote device through the socket. This call blocks
+                // until it succeeds or throws an exception.
+                mmSocket.connect();
+                Log.e("Status", "Device connected");
+                handler.obtainMessage(CONNECTING_STATUS, 1, -1).sendToTarget();
+            } catch (IOException connectException) {
+                // Unable to connect; close the socket and return.
+                try {
+                    mmSocket.close();
+                    Log.e("Status", "Cannot connect to device");
+                    handler.obtainMessage(CONNECTING_STATUS, -1, -1).sendToTarget();
+                } catch (IOException closeException) {
+                    Log.e(TAG, "Could not close the client socket", closeException);
+                }
+                return;
+            }
         }
     }
 
+    private class ConnectedThread extends Thread {
+
+        private final InputStream inputStream;
+        private final OutputStream outputStream;
+
+        public ConnectedThread(BluetoothSocket bluetoothSocket) {
+            InputStream inputStream = null;
+            OutputStream outputStream = null;
+
+            try {
+                inputStream = bluetoothSocket.getInputStream();
+                outputStream = bluetoothSocket.getOutputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            this.inputStream = inputStream;
+            this.outputStream = outputStream;
+        }
+
+        @Override
+        public void run() {
+
+        }
+
+        public void write(String command) {
+            byte[] bytes = command.getBytes();
+            if (outputStream != null) {
+                try {
+                    outputStream.write(bytes);
+                    outputStream.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        public void cancel() {
+            try {
+                inputStream.close();
+                outputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
