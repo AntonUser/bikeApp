0e8dc480c13ed8350bc98743b91fb68661b65c6e
Anton Titov <titovanton5@mail.ru>
2022-07-01 16:31:34 +0600

    feat: add VK sdk dependencies

--- app/src/main/java/com/example/bikeapp/ProfileFragment.java
+++ app/src/main/java/com/example/bikeapp/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.bikeapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,7 +9,10 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.vk.api.sdk.utils.VKUtils;
 
 public class ProfileFragment extends Fragment {
 
@@ -19,20 +23,15 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-    }
+        }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
-        WebView webView = (WebView) view.findViewById(R.id.vk_web_view);
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("https://vk.com/marsbikeru");
+
         return view;
     }
 
-}
+   }
